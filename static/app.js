559ea05258d0aa233246e520818860028218427e
Caresle <carabesle@gmail.com>
2021-09-25 17:49:54 -0600

    Add support for light and dark theme

--- static/app.js
+++ static/app.js
@@ -1,6 +1,11 @@
-const buttonTheme = document.getElementById('change-theme')
 const body = document.body
+
+const buttonTheme = document.getElementById('change-theme')
+const learnMore = document.getElementById('learn-more')
+const buttonContainer = document.getElementById('buttons-container')
+
 let changeTheme = false
+let timeoutId
 
 buttonTheme.addEventListener('click', () => {
 	changeTheme = !changeTheme
@@ -12,3 +17,13 @@ buttonTheme.addEventListener('click', () => {
 		buttonTheme.innerHTML = 'Dark theme'
 	}
 })
+
+learnMore.addEventListener('click', () => {
+	buttonContainer.classList.add('btn-hide')
+	timeoutId = setTimeout(removeButtons, 300)
+})
+
+function removeButtons() {
+	buttonContainer.remove()
+	clearTimeout(timeoutId)
+}
